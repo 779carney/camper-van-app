3011296aa29b14a8839f70c7cc2df2b9798ce87c
Sam Gower <gowersam@icloud.com>
2023-06-13 15:01:43 +0100

    starting homepage

--- front-end/pages/Home.jsx
+++ front-end/pages/Home.jsx
@@ -1,14 +1,20 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, ScrollView, Image } from "react-native";
 
 function Home() {
   const { navigate } = useNavigation();
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
       <Button
+        color="#f194ff"
         title="Advertise a Van"
         onPress={() => navigate("AdvertiseVan")}
+      />
+      <Button
+        color="#f194ff"
+        title="View all Vans"
+        onPress={() => navigate("Vans")}
       />
     </View>
   );
